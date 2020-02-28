d924cec2c6736393856b666494a815301b7cdda5
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-28 14:47:23 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :sightings, only: [:index, :show]
+  # get '/sightings' => 'sightings#index'
+  # get '/sightings/:id' => 'sightings#show'
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show'
 end
